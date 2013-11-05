3caff18ad2c81d896bc0e75cd815badf8bfa500c
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2013-11-04 22:33:27 -0800

    Creating all Post resource with model backed forms to create and edit post objects

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,41 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
     
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Your post was updated."
+      redirect_to post_path
+    else
+      render 'edit'
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit!
   end
 end
